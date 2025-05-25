a7a0aeb7298ca25e1bf0cde0f1add6ca1a684ef9
Paul Olteanu <p.a.olteanu@gmail.com>
2025-05-24 23:36:06 -0400

    update jj config

--- home/modules/jj.nix
+++ home/modules/jj.nix
@@ -7,8 +7,50 @@
   programs.jujutsu.settings = {
     user.name = "Paul Olteanu";
     user.email = "p.a.olteanu@gmail.com";
-    ui.pager = "less -FRX";
 
-    aliases.lg = ["log"];
+    git = {
+      write-change-id-header = true;
+      subprocess = true;
+    };
+
+    ui = {
+      default-command = "status";
+      pager = "less -FRX";
+      diff.format = "git";
+      diff.tool = ["difft" "--color=always" "$left" "$right"];
+    };
+
+    aliases = {
+      lg = ["log" "-T" "log_compact"];
+      tug = ["bookmark" "move" "--from" "heads(::@- & bookmarks())" "--to" "@-"];
+    };
+
+    template-aliases = {
+      log_compact = ''
+        if(root,
+          format_root_commit(self),
+          label(if(current_working_copy, "working_copy"),
+            concat(
+              separate(" ",
+                pad_end(4, format_short_change_id_with_hidden_and_divergent_info(self)),
+                if(empty, label("empty", "(empty)")),
+                if(description,
+                  description.first_line(),
+                  label(if(empty, "empty"), description_placeholder),
+                ),
+                bookmarks,
+                tags,
+                working_copies,
+                if(git_head, label("git_head", "HEAD")),
+                if(conflict, label("conflict", "conflict")),
+                if(config("ui.show-cryptographic-signatures").as_boolean(),
+                  format_short_cryptographic_signature(signature)),
+                format_timestamp(commit_timestamp(self)),
+              ) ++ "\n",
+            ),
+          )
+        )
+      '';
+    };
   };
 }
